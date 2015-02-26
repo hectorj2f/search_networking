36e4bff60477bf27cf013ca9dc7ff76931edd51c
Hector Fernandez <hector@elasticbox.com>
2015-02-26 23:15:43 +0100

    Discovery unit template and close transport after every message.

--- networking/server.go
+++ networking/server.go
@@ -6,46 +6,17 @@ import (
   "net"
   "fmt"
   "strconv"
+  "time"
 
   "github.com/hectorj2f/search_networking/api"
   "github.com/hectorj2f/search_networking/networking/serializer"
+  "github.com/hectorj2f/search_networking/resources"
   "github.com/hectorj2f/search_networking/types"
 
   "github.com/docker/libchan"
   "github.com/docker/libchan/spdy"
   logger "github.com/Sirupsen/logrus"
 )
-
-func startListener(cert string, key string, port int) (net.Listener, error){
-  var listener net.Listener
-
-  if cert != "" && key != "" {
-    tlsCert, err := tls.LoadX509KeyPair(cert, key)
-    if err != nil {
-      return nil, err
-    }
-
-    tlsConfig := &tls.Config{
-      InsecureSkipVerify: true,
-      Certificates:       []tls.Certificate{tlsCert},
-    }
-
-    listener, err = tls.Listen("tcp", fmt.Sprintf(":%s", strconv.Itoa(port)), tlsConfig)
-    if err != nil {
-      return nil, err
-    }
-  } else {
-    var err error
-    logger.Info("TLS configuration is disabled")
-    listener, err = net.Listen("tcp", fmt.Sprintf(":%s", strconv.Itoa(port)))
-    if err != nil {
-      return nil, err
-    }
-  }
-  logger.Infof("Listening at %s...\n", strconv.Itoa(port))
-  return listener, nil
-}
-
 
 func Server(cert string, key string, port int) error {
   listener, err := startListener(cert, key, port)
@@ -75,14 +46,17 @@ func Server(cert string, key string, port int) error {
 
     // Go routine to wait for incoming messages
     go waitForMessages(receiver, transport)
+    time.Sleep(500 * time.Millisecond)
+    transport.Close()
 
   }
   return nil
 }
 
+
 func waitForMessages(receiver libchan.Receiver, transport *spdy.Transport) error {
   for {
-      logger.Info("Waiting for connection...")
+      logger.Info("Waiting for a message...")
       message := &types.Message{}
       err := receiver.Receive(message)
       if err != nil && err != io.EOF {
@@ -95,8 +69,9 @@ func waitForMessages(receiver libchan.Receiver, transport *spdy.Transport) error
           logger.Error(err)
         }
       } else {
-        logger.Debug("Message received!")
         logger.Debug(message)
+        // Register the operation
+        api.RegisterRequest(resources.PROCESSING_STATE, message.Id.String())
 
         if len(message.Data) < 1 {
           logger.Errorf("Unexpected message length: %s", strconv.Itoa(len(message.Data)))
@@ -109,14 +84,49 @@ func waitForMessages(receiver libchan.Receiver, transport *spdy.Transport) error
         }
         data, err := serializer.EncodeArrayMsgPack(users)
 
+        // TODO: Perhaps we should send the response once the receiver is alive and ready
         ack := &types.AckMessage{Id: message.Id, Data: data}
-        logger.Debug("Sending ack...")
         err = message.Ret.Send(ack)
         logger.Debug("Sent ack!")
+
+        // Update the state of the request
+        api.UpdateRequest(resources.DONE_STATE, message.Id.String())
+
         if err != nil {
           logger.Errorf("Error sending ack: %s", err)
         }
       }
   }
 
+}
+
+
+func startListener(cert string, key string, port int) (net.Listener, error){
+  var listener net.Listener
+
+  if cert != "" && key != "" {
+    tlsCert, err := tls.LoadX509KeyPair(cert, key)
+    if err != nil {
+      return nil, err
+    }
+
+    tlsConfig := &tls.Config{
+      InsecureSkipVerify: true,
+      Certificates:       []tls.Certificate{tlsCert},
+    }
+
+    listener, err = tls.Listen("tcp", fmt.Sprintf(":%s", strconv.Itoa(port)), tlsConfig)
+    if err != nil {
+      return nil, err
+    }
+  } else {
+    var err error
+    logger.Info("TLS configuration is disabled")
+    listener, err = net.Listen("tcp", fmt.Sprintf(":%s", strconv.Itoa(port)))
+    if err != nil {
+      return nil, err
+    }
+  }
+  logger.Infof("Listening at %s...\n", strconv.Itoa(port))
+  return listener, nil
 }
